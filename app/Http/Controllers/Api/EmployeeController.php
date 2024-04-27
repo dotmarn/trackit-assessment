30264d82662b9ad78e097ec7d426930b2c7446e9
Kasim Ridwan <kasimsheyi20@gmail.com>
2024-04-27 17:50:12 +0100

    clean up controller

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\DTO\EmployeeDTO;
+use App\Services\TrackTik;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\EmployeeRequest;
 
 class EmployeeController extends Controller
 {
-    public function __invoke(EmployeeRequest $request)
+    public function __invoke(EmployeeRequest $request, TrackTik $trackTik) : \Illuminate\Http\JsonResponse
     {
-        $payload = (new EmployeeDTO($request->validated()))->mapData();
-
-        return $payload;        
+        $payload = new EmployeeDTO($request->validated()); 
+        $response = $request->method() === 'POST' ? $trackTik->create($payload) : $trackTik->update($payload);
+        return $response;
     }
 
 }
